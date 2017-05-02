cb26f174f4989f7fd18a3f25787e79bdc407141a
Laura Stelzner <laura.stelzner@tri.global>
2017-05-02 11:04:52 -0700

    removing unnecessary save

--- src/task_behavior_engine/tree.py
+++ src/task_behavior_engine/tree.py
@@ -333,7 +333,6 @@ class Node(object):
             self._cancel()
         if self._cleanup_cb:
             nodedata = self._blackboard.get_memory(self._id)
-            nodedata.set_data('last_result', self._result)
             self._cleanup_cb(nodedata)
         self._force_state = None
         self._result = NodeStatus(
