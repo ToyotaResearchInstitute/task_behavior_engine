b753a7bdf0eb9eb729680a0cd09a44d18facb32a
Martin Ross <contactmartinross@yahoo.co.uk>
2021-04-23 18:04:06 +0100

    Updated package.xml with README and setup.py with classifiers and install_requires

--- setup.py
+++ setup.py
@@ -7,6 +7,14 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['task_behavior_engine',],
-    package_dir={'': 'src'})
+    package_dir={'': 'src'},
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=[
+        'six'
+    ],
+)
 
 setup(**setup_args)
